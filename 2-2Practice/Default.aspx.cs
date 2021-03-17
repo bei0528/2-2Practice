2c1e533351fe9ab0ae8a95db98fc0800bf4265a8
欣蓓 吳 <107111223@mail.oit.edu.tw>
2021-03-18 00:35:56 +0800

    2-2練習

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,34 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            int i_n = 1000;
+            bool a_IsLeap = false;
 
+            if (i_n % 4 == 0)
+            {
+                a_IsLeap = true;
+
+                if (i_n % 100 == 0)
+                {
+                    if (i_n % 400 == 0)
+                    {
+                        a_IsLeap = true;
+                    }
+                    else
+                    {
+                        a_IsLeap = false;
+                    }
+                }
+            }
+
+            if (a_IsLeap == true)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
